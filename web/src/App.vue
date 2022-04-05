05d91dcabebabe2b39e11543f87b5c211554eff7
unknown <melanycristyg@gmail.com>
2022-04-05 10:41:02 +0200

    first commit, new web design

---
+++ web/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Panda logo" src="./assets/pandita.png">
+  <MainPage msg="Welcome to the landing page!"/>
+</template>
+
+<script>
+import MainPage from './components/MainPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
