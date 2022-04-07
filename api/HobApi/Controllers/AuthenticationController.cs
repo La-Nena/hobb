f07e8d6318de55a472dd4b1765b129f7c9315a18
Melany Gomez <melanycristyg@gmail.com>
2022-04-07 13:40:46 +0200

    api set, backend set, login post request set

---
+++ api/HobApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,25 @@
+﻿using HobApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HobApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(LoginRequest loginRequest)
+        {
+            if (loginRequest.Email == "bobbo@bobbo.com" && loginRequest.Password == "bobba")
+            {
+                return Ok("YOU MADE IT!!!!");
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
--- web/src/components/LoginPage.vue
+++ web/src/components/LoginPage.vue
@@ -2,24 +2,50 @@
  <form>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input v-model="password" type="password" class="form-control" id="exampleInputPassword1">
   </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <div v-if="incorrectCredentials">YOU ENTERED WRONG CREDENTIALS IDIOT</div>
+  <button type="button" class="btn btn-primary" @click="login()">Submit</button>
 </form> 
 </template>
 
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: "LoginPage"
+  name: "LoginPage",
+  data() {
+    return {
+      email: '',
+      password: '',
+      incorrectCredentials: false
+    };
+  },
+  methods: {
+    login(){
+      axios.post('https://localhost:7254/api/Authentication/login', {
+        email: this.email,
+        password: this.password
+      })
+      .then(() => {
+        console.log('IT WORKED');
+      })
+      .catch(() => {
+        this.incorrectCredentials = true;
+        console.log('IT DIDNT WORK :(');
+      })
+      .finally(() => {
+        this.email = '';
+        this.password = '';
+        console.log('YOU ARE AWESOME, EVEN IF YOU FAILED OR NOT.');
+      });
+    }
+  }
 }
 </script>
